8a4ab39642583b631c28dac955245118bbd4ac6d
Young Bin Kim <lyakyb@gmail.com>
2015-04-22 10:47:02 -0600

    Initial commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour
+{
+	float mousePositionInBlocks;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		mousePositionInBlocks = Input.mousePosition.x / Screen.width * 16;
+		var paddlePosition = new Vector3 (0.5f, this.transform.position.y, 0f);
+
+		paddlePosition.x = Mathf.Clamp (mousePositionInBlocks, 0.5f, 15.5f);
+
+		this.transform.position = paddlePosition;
+	}
+}
